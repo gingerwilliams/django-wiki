0b75468a215649587d0b1aa915a9b62a939e558e
ginger <geedimensions@gmail.com>
2025-01-03 22:10:17 -0500

    init

---
+++ encyclopedia/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+from . import util
+
+
+def index(request):
+    return render(request, "encyclopedia/index.html", {
+        "entries": util.list_entries()
+    })
+
+def entry(request):
+    return HttpResponse("Hello, World!")
+
